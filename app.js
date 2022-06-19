b24e51d67622abd4683bfdd2208352f19577e8c1
Frodo <frodo@github>
2022-06-19 10:43:30 +0300

    Passport, LocalStrategy, bcryptjs Authentication practice

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require('path');
 require('dotenv').config();
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/user');
+const bcrypt = require('bcryptjs');
 
 global.appRoot = path.resolve(__dirname);
 global.uploadDirectoryRelative = process.env.UPLOAD_DIRECTORY;
@@ -15,12 +20,49 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: "Incorrect password" })
+            }
+        });
+      });
+    })
+);
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 app.use('/', require('./routes/index'));
 app.use('/collection', require('./routes/collection'));
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require("passport");
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
     res.render('home', { title: "This is the homepage" });
 });
+router.get("/log-out", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+});
+router.get('/sign-up', (req, res) => {
+    res.render('sign_up', { title: "This is the Sign Up page" });
+});
+router.post(
+    "/",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
+router.post('/sign-up', (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) { return next(err); }
+        const user = new User({
+            username: req.body.username,
+            password: hashedPassword
+          }).save(err => {
+            if (err) { 
+              return next(err);
+            }
+            res.redirect("/");
+        });
+    });
 
+});
 module.exports = router;
